3176588803f9f20faa06e3afa5615667defe86d2
ibrzrk <mrkiblingz@gmail.com>
2024-12-03 21:58:40 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace ELECTION_DATA_CIS_PROJECT
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(448, 147);
+            this.txt1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(178, 26);
+            this.txt1.TabIndex = 0;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label1.Location = new System.Drawing.Point(8, 142);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(436, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ENTER VALID ELECTION YEAR:";
+            // 
+            // dgv
+            // 
+            this.dgv.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(15, 206);
+            this.dgv.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(733, 544);
+            this.dgv.TabIndex = 2;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS PGothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label2.Location = new System.Drawing.Point(8, 175);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(401, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "DATA FOR YEAR SELECTED:";
+            // 
+            // pic
+            // 
+            this.pic.InitialImage = null;
+            this.pic.Location = new System.Drawing.Point(831, 206);
+            this.pic.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(756, 395);
+            this.pic.TabIndex = 4;
+            this.pic.TabStop = false;
+            this.pic.Click += new System.EventHandler(this.pic_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS PGothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label3.Location = new System.Drawing.Point(1113, 175);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = " DATA MAP:";
+            // 
+            // btn1
+            // 
+            this.btn1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btn1.Location = new System.Drawing.Point(15, 13);
+            this.btn1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(159, 54);
+            this.btn1.TabIndex = 6;
+            this.btn1.Text = "EXIT";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS PGothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label4.Location = new System.Drawing.Point(403, 175);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "PRESIDENT:";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("MS PGothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.lbl1.Location = new System.Drawing.Point(571, 175);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(169, 29);
+            this.lbl1.TabIndex = 9;
+            this.lbl1.Text = "PRESIDENT";
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1666, 753);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "form1";
+            this.Text = "ELECTION DATA";
+            this.Load += new System.EventHandler(this.form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,387 @@
+﻿// Author      : Noah Lancaster and Bryant Alarcon
+// Date        : 2024.12.02
+// Email       : mrkiblingz@gmail.com
+// Assignment  : FINAL PROJECT: We have created a simple data base that allows users to see the state data for Presidential elections from 2020-1992
+// Des         : Change to Exact Cents
+// Pledge      : As a Falcon @ Solano College, I will conduct myself with honor and integrity at all 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
+
+namespace ELECTION_DATA_CIS_PROJECT
+{
+    public partial class form1 : Form
+    {
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+            string year = txt1.Text;
+            if (year == "2020")
+            {
+                //our code can only run with correct directory we are using flash drive
+                //we will include video of our code running
+                DataTable dt = new DataTable();
+                Directory.SetCurrentDirectory(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\2020");
+                string fpath = Directory.GetCurrentDirectory();
+                string[] csv_file = Directory.GetFiles(fpath, "*.data");
+
+                //stream reader
+                StreamReader sr = new StreamReader(csv_file[0].ToString());
+                //categorizes data so it can be read as follows
+                dt.Columns.Add("State");
+                dt.Columns.Add("Total Votes");
+                dt.Columns.Add("BIDEN Votes");
+                dt.Columns.Add("BIDEN %");
+                dt.Columns.Add("BIDEN EV");
+                dt.Columns.Add("TRUMP Votes");
+                dt.Columns.Add("TRUMP %");
+                dt.Columns.Add("TRUMP EV");
+                dt.Columns.Add("OTHER Votes");
+                dt.Columns.Add("OTHER %");
+                dt.Columns.Add("OTHER EV");
+                string[] data = new string[File.ReadAllLines(csv_file[0]).Length];
+                data = sr.ReadLine().Split(',');
+                while (!sr.EndOfStream)
+                {
+                    data = sr.ReadLine().Split(',');
+                    dt.Rows.Add(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10]); //creates data tables
+                }
+                dgv.DataSource = dt;
+            }
+            else if (year == "2016")
+            {
+                DataTable dt = new DataTable();
+                Directory.SetCurrentDirectory(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\2016");
+                string fpath = Directory.GetCurrentDirectory();
+                string[] csv_file = Directory.GetFiles(fpath, "*.data");
+
+                //stream reader
+                StreamReader sr = new StreamReader(csv_file[0].ToString());
+                dt.Columns.Add("State");
+                dt.Columns.Add("Total Votes");
+                dt.Columns.Add("CLINTON Votes");
+                dt.Columns.Add("CLINTON %");
+                dt.Columns.Add("CLINTON EV");
+                dt.Columns.Add("TRUMP Votes");
+                dt.Columns.Add("TRUMP %");
+                dt.Columns.Add("TRUMP EV");
+                dt.Columns.Add("JOHNSON Votes");
+                dt.Columns.Add("JOHNSON %");
+                dt.Columns.Add("JOHNSON EV");
+                string[] data = new string[File.ReadAllLines(csv_file[0]).Length];
+                data = sr.ReadLine().Split(',');
+                while (!sr.EndOfStream)
+                {
+                    data = sr.ReadLine().Split(',');
+                    dt.Rows.Add(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10]);
+                }
+                dgv.DataSource = dt;
+            }
+            else if (year == "2012")
+            {
+                DataTable dt = new DataTable();
+                Directory.SetCurrentDirectory(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\2012");
+                string fpath = Directory.GetCurrentDirectory();
+                string[] csv_file = Directory.GetFiles(fpath, "*.data");
+
+                //stream reader
+                StreamReader sr = new StreamReader(csv_file[0].ToString());
+                dt.Columns.Add("State");
+                dt.Columns.Add("Total Votes");
+                dt.Columns.Add("OBAMA Votes");
+                dt.Columns.Add("OBAMA %");
+                dt.Columns.Add("OBAMA EV");
+                dt.Columns.Add("ROMNEY Votes");
+                dt.Columns.Add("ROMNEY %");
+                dt.Columns.Add("ROMNEY EV");
+                string[] data = new string[File.ReadAllLines(csv_file[0]).Length];
+                data = sr.ReadLine().Split(',');
+                while (!sr.EndOfStream)
+                {
+                    data = sr.ReadLine().Split(',');
+                    dt.Rows.Add(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
+                }
+                dgv.DataSource = dt;
+            }
+            else if (year == "2008")
+            {
+                DataTable dt = new DataTable();
+                Directory.SetCurrentDirectory(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\2008");
+                string fpath = Directory.GetCurrentDirectory();
+                string[] csv_file = Directory.GetFiles(fpath, "*.data");
+
+                //stream reader
+                StreamReader sr = new StreamReader(csv_file[0].ToString());
+                dt.Columns.Add("State");
+                dt.Columns.Add("Total Votes");
+                dt.Columns.Add("OBAMA Votes");
+                dt.Columns.Add("OBAMA %");
+                dt.Columns.Add("OBAMA EV");
+                dt.Columns.Add("McCAIN Votes");
+                dt.Columns.Add("McCAIN %");
+                dt.Columns.Add("McCAIN EV");
+                string[] data = new string[File.ReadAllLines(csv_file[0]).Length];
+                data = sr.ReadLine().Split(',');
+                while (!sr.EndOfStream)
+                {
+                    data = sr.ReadLine().Split(',');
+                    dt.Rows.Add(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
+                }
+                dgv.DataSource = dt;
+            }
+            else if (year == "2004")
+            {
+                DataTable dt = new DataTable();
+                Directory.SetCurrentDirectory(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\2004");
+                string fpath = Directory.GetCurrentDirectory();
+                string[] csv_file = Directory.GetFiles(fpath, "*.data");
+
+                //stream reader
+                StreamReader sr = new StreamReader(csv_file[0].ToString());
+                dt.Columns.Add("State");
+                dt.Columns.Add("Total Votes");
+                dt.Columns.Add("BUSH Votes");
+                dt.Columns.Add("BUSH %");
+                dt.Columns.Add("BUSH EV");
+                dt.Columns.Add("KERRY Votes");
+                dt.Columns.Add("KERRY %");
+                dt.Columns.Add("KERRY EV");
+                string[] data = new string[File.ReadAllLines(csv_file[0]).Length];
+                data = sr.ReadLine().Split(',');
+                while (!sr.EndOfStream)
+                {
+                    data = sr.ReadLine().Split(',');
+                    dt.Rows.Add(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
+                }
+                dgv.DataSource = dt;
+            }
+            else if (year == "2000")
+            {
+                DataTable dt = new DataTable();
+                Directory.SetCurrentDirectory(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\2000");
+                string fpath = Directory.GetCurrentDirectory();
+                string[] csv_file = Directory.GetFiles(fpath, "*.data");
+
+                //stream reader
+                StreamReader sr = new StreamReader(csv_file[0].ToString());
+                dt.Columns.Add("State");
+                dt.Columns.Add("Total Votes");
+                dt.Columns.Add("BUSH Votes");
+                dt.Columns.Add("BUSH %");
+                dt.Columns.Add("BUSH EV");
+                dt.Columns.Add("GORE Votes");
+                dt.Columns.Add("GORE %");
+                dt.Columns.Add("GORE EV");
+                dt.Columns.Add("NADER Votes");
+                dt.Columns.Add("NADER %");
+                dt.Columns.Add("NADER EV");
+                string[] data = new string[File.ReadAllLines(csv_file[0]).Length];
+                data = sr.ReadLine().Split(',');
+                while (!sr.EndOfStream)
+                {
+                    data = sr.ReadLine().Split(',');
+                    dt.Rows.Add(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10]);
+                }
+                dgv.DataSource = dt;
+            }
+            else if (year == "1996")
+            {
+                DataTable dt = new DataTable();
+                Directory.SetCurrentDirectory(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\1996");
+                string fpath = Directory.GetCurrentDirectory();
+                string[] csv_file = Directory.GetFiles(fpath, "*.data");
+
+                //stream reader
+                StreamReader sr = new StreamReader(csv_file[0].ToString());
+                dt.Columns.Add("State");
+                dt.Columns.Add("Total Votes");
+                dt.Columns.Add("Clinton Votes");
+                dt.Columns.Add("Clinton %");
+                dt.Columns.Add("Clinton EV");
+                dt.Columns.Add("BUSH Votes");
+                dt.Columns.Add("BUSH %");
+                dt.Columns.Add("BUSH EV");
+                dt.Columns.Add("PEROT Votes");
+                dt.Columns.Add("PEROT %");
+                dt.Columns.Add("PEROT EV");
+                string[] data = new string[File.ReadAllLines(csv_file[0]).Length];
+                data = sr.ReadLine().Split(',');
+                while (!sr.EndOfStream)
+                {
+                    data = sr.ReadLine().Split(',');
+                    dt.Rows.Add(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10]);
+                }
+                dgv.DataSource = dt;
+            }
+            else if (year == "1992")
+            {
+                DataTable dt = new DataTable();
+                Directory.SetCurrentDirectory(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\1992");
+                string fpath = Directory.GetCurrentDirectory();
+                string[] csv_file = Directory.GetFiles(fpath, "*.data");
+
+                //stream reader
+                StreamReader sr = new StreamReader(csv_file[0].ToString());
+                dt.Columns.Add("State");
+                dt.Columns.Add("Total Votes");
+                dt.Columns.Add("Clinton Votes");
+                dt.Columns.Add("Clinton %");
+                dt.Columns.Add("Clinton EV");
+                dt.Columns.Add("BUSH Votes");
+                dt.Columns.Add("BUSH %");
+                dt.Columns.Add("BUSH EV");
+                dt.Columns.Add("PEROT Votes");
+                dt.Columns.Add("PEROT %");
+                dt.Columns.Add("PEROT EV");
+                string[] data = new string[File.ReadAllLines(csv_file[0]).Length];
+                data = sr.ReadLine().Split(',');
+                while (!sr.EndOfStream)
+                {
+                    data = sr.ReadLine().Split(',');
+                    dt.Rows.Add(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10]);
+                }
+                dgv.DataSource = dt;
+            }
+
+            {
+                if (year == "2020")
+                {
+
+                    pic.Image = Image.FromFile(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\2020map\2020.jpg");
+                }
+                else if (year == "2016")
+                {
+                    PictureBox dt = new PictureBox();
+                    pic.Image = Image.FromFile(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\2016Map\2016.jpg");
+                }
+                else if (year == "2012")
+                {
+                    PictureBox dt = new PictureBox();
+                    pic.Image = Image.FromFile(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\2012map\2012.jpg");
+                }
+                else if (year == "2008")
+                {
+                    PictureBox dt = new PictureBox();
+                    pic.Image = Image.FromFile(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\2008map\2008.jpg");
+                }
+                else if (year == "2004")
+                {
+                    PictureBox dt = new PictureBox();
+                    pic.Image = Image.FromFile(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\2004map\2004.jpg");
+                }
+                else if (year == "2000")
+                {
+                    PictureBox dt = new PictureBox();
+                    pic.Image = Image.FromFile(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\2000map\2000.jpg");
+                }
+                else if (year == "1996")
+                {
+                    PictureBox dt = new PictureBox();
+                    pic.Image = Image.FromFile(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\1996map\1996.jpg");
+                }
+                else if (year == "1992")
+                {
+                    PictureBox dt = new PictureBox();
+                    pic.Image = Image.FromFile(@"E:\CIS22 PROJECT\ELECTION DATA CIS PROJECT\1992map\1992.jpg");
+                }
+                {
+                }
+
+                {
+                    if (year == "2020")
+                    {
+                        lbl1.Text = ("Democratic Joe Biden");
+                    }
+                    else if (year == "2016")
+                    {
+                        lbl1.Text = ("Republican Donald Trump");
+                    }
+                    else if (year == "2012")
+                    {
+                        lbl1.Text = ("Democratic Barack Obama");
+
+                    }
+                    else if (year == "2008")
+                    {
+                        lbl1.Text = ("Democratic Barack Obama");
+
+                    }
+                    else if (year == "2004")
+                    {
+                        lbl1.Text = ("Republican George W. Bush");
+
+
+                    }
+                    else if (year == "2000")
+                    {
+                        lbl1.Text = ("Republican George W. Bush");
+
+                    }
+                    else if (year == "1996")
+                    {
+                        lbl1.Text = ("Democratic William J. Clinton");
+
+                    }
+                    else if (year == "1992")
+                    {
+                        lbl1.Text = ("Democratic William J. Clinton");
+
+                    }
+                }
+            }
+        }
+            
+           
+            
+            
+            
+
+
+           
+        
+        private void form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void pic_Click(object sender, EventArgs e)
+        {
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+            
+
+            }
+        }
+    }
+
+
+// SOURCE: https://www.presidency.ucsb.edu/statistics/elections/1992 used to get data everything else we learned in class and used it in our final project.
+
+
